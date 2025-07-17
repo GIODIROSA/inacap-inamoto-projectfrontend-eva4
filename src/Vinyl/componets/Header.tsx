71212d7c36ae8be79298a6867bdc631e1334dbff
GIODIROSA <giodirosaartes@gmail.com>
2025-07-16 21:33:57 -0400

    [ADD] Logo added

--- src/Vinyl/componets/Header.tsx
+++ src/Vinyl/componets/Header.tsx
@@ -1,14 +1,22 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom'; 
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import LogoInamoto from "../assets/img/vinilo.png";
 
 const Header: React.FC = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand as={NavLink} to="/">Mi Aplicación</Navbar.Brand>
+      <Container className="d-flex justify-content-between align-items-center">
+        <Navbar.Brand as={NavLink} to="/" className="d-flex align-items-center">
+          <img
+            src={LogoInamoto}
+            alt="imagotipo Inamoto"
+            style={{ height: "30px", marginRight: "10px" }}
+          />
+          Inamoto
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link as={NavLink} to="/" end>
               Inicio
             </Nav.Link>
